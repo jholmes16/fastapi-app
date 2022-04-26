2bf23991e58135cbcaa79059672b958ea76065d7
jholmes16 <jholmesarea@gmail.com>
2022-04-26 13:04:45 -0400

    start users post method in app

--- app.py
+++ app.py
@@ -2,4 +2,13 @@ import fastapi as _fastapi
 import fastapi.security as _security
 import sqlalchemy.orm as _orm
 
+import schemas as _schemas
+import services as _services
+
 app = _fastapi.FastAPI()
+
+@app.post("/api/v1/users")
+async def register_user(
+    user: _schemas.UserRequest, db: _orm.Session = _fastapi.Depends(_services.get_db())
+):
+    pass
