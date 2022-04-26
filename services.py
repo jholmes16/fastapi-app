9ca74b53212e4e1b4abbf04b8fe07903e7a8eb19
jholmes16 <jholmesarea@gmail.com>
2022-04-26 13:46:17 -0400

    create a user in db

--- services.py
+++ services.py
@@ -1,6 +1,10 @@
 import database as _database
 import models as _models
 import sqlalchemy.orm as _orm
+import schemas as _schemas
+import email_validator as _email_validator
+import fastapi as _fastapi
+import passlib.hash as _hash
 
 def create_db():
     return _database.Base.metadata.create_all(bind=_database.engine)
@@ -14,3 +18,26 @@ def get_db():
 
 async def getUserByEmail(email: str, db: _orm.Session):
     return db.query(_models.UserModel).filter(_models.UserModel.email == email).first()
+
+async def create_user(user: _schemas.UserRequest, db: _orm.Session)
+    # check for valid Email
+    try:
+        isValid = _email_validator.validate_email(email = user.email)
+        email = isValid.email
+    except _email_validator.EmailNotValidError:
+        raise _fastapi.HTTPException(status_code = 400, detail = "Provide valid Email")
+
+    # convert normal password to hash form
+    hashed_password = hash.bcrypt.hash(user.password)
+    # create the user model to be saved in database
+    user_obj = models.UserModel(
+        email = email,
+        name = user.name,
+        phone = user.phone,
+        password_hash = hashed_password
+    )
+    #save the user in the db
+    db.add(user_obj)
+    db.commit()
+    db.refresh(user_obj)
+    return user_obj
